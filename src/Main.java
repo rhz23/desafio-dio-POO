561bbc3eb18574e8888fb232e22d8456c9901098
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-12-22 16:59:18 -0300

    primeiras implementações Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Js");
+        curso2.setDescricao("descrição curso Js");
+        curso2.setCargaHoraria(8);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("descrição mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+
+    }
+}
